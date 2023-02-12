619e557bf6b01ad01dd25f85a6105cee380059f8
azozdw <219110368@psu.edu.sa>
2023-02-12 11:41:16 +0300

    after adding banner

--- jenkin.java
+++ jenkin.java
@@ -3,8 +3,11 @@ package miscelleneous;
 public class jenkin{
     public static void main(String[] args) {
         int Char=0;
-        for (int i = 0; i< 256; i++, Char++){
+		int s = 10;
+        for (int i = 0; i< 200; i++, Char++){
             System.out.print((char)i+((Char%10==0)?"\n":"\t"));
+		System.out.print((s)i+((s%10==0)?"\n":"\t"));
+
         }
     }
 }
